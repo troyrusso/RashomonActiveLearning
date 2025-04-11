cd4d7a9a1f10a49ae1bcd7e2336f1ffacc1b82f8
simondn <simonnguyen1999@gmail.com>
2025-04-11 10:20:04 -0700

    Expand dataset

--- Code/utils/Auxiliary/LoadAnalyzedData.py
+++ Code/utils/Auxiliary/LoadAnalyzedData.py
@@ -1,6 +1,7 @@
 
 ### Packages ###
 import os
+import pickle
 import pandas as pd
 
 ### Function ###
@@ -9,35 +10,20 @@ def LoadAnalyzedData(data_type, base_directory, method, parameter):
     ResultsDirectory = os.path.join(base_directory, data_type, method)
 
     ### File Path ###
-
-
     if method == "RandomForestClassification":
         PathTemplates = {
             "Error": f"ProcessedResults/ErrorVec/{parameter}_ErrorMatrix.csv",
             "Time": f"ProcessedResults/ElapsedTime/{parameter}_TimeMatrix.csv",
-            "SelectionHistory_RF": f"ProcessedResults/SelectionHistory/{parameter}_SelectionHistory.csv"
+            "SelectionHistory_RF": f"ProcessedResults/SelectionHistory/{parameter}_SelectionHistory.pkl"
         }
-    # if method == "TreeFarms":
-    #     PathTemplates = {
-    #         "Error_UNREAL": f"ProcessedResults/ErrorVec/UNQ{parameter}_ErrorMatrix.csv",
-    #         "Error_DUREAL": f"ProcessedResults/ErrorVec/DPL{parameter}_ErrorMatrix.csv",
-    #         "Time_UNREAL": f"ProcessedResults/ElapsedTime/UNQ{parameter}_TimeMatrix.csv",
-    #         "Time_DUREAL": f"ProcessedResults/ElapsedTime/DPL{parameter}_TimeMatrix.csv",
-    #         "SelectionHistory_UNREAL": f"ProcessedResults/SelectionHistory/UNQ{parameter}_SelectionHistory.csv",
-    #         "SelectionHistory_DUREAL": f"ProcessedResults/SelectionHistory/DPL{parameter}_SelectionHistory.csv",
-    #         "TreeCounts_UNIQUE_UNREAL": f"ProcessedResults/TreeCount/UNQ{parameter}_UniqueTreeCount.csv",
-    #         "TreeCounts_UNIQUE_DUREAL": f"ProcessedResults/TreeCount/DPL{parameter}_UniqueTreeCount.csv",
-    #         "TreeCounts_ALL_UNREAL": f"ProcessedResults/TreeCount/UNQ{parameter}_AllTreeCount.csv",
-    #         "TreeCounts_ALL_DUREAL": f"ProcessedResults/TreeCount/DPL{parameter}_AllTreeCount.csv",
-    #     }
     if method == "TreeFarms":
         PathTemplates = {
             "Error_UNREAL": f"ProcessedResults/ErrorVec/UA{parameter}_ErrorMatrix.csv",
             "Error_DUREAL": f"ProcessedResults/ErrorVec/DA{parameter}_ErrorMatrix.csv",
             "Time_UNREAL": f"ProcessedResults/ElapsedTime/UA{parameter}_TimeMatrix.csv",
             "Time_DUREAL": f"ProcessedResults/ElapsedTime/DA{parameter}_TimeMatrix.csv",
-            "SelectionHistory_UNREAL": f"ProcessedResults/SelectionHistory/UA{parameter}_SelectionHistory.csv",
-            "SelectionHistory_DUREAL": f"ProcessedResults/SelectionHistory/DA{parameter}_SelectionHistory.csv",
+            "SelectionHistory_UNREAL": f"ProcessedResults/SelectionHistory/UA{parameter}_SelectionHistory.pkl",
+            "SelectionHistory_DUREAL": f"ProcessedResults/SelectionHistory/DA{parameter}_SelectionHistory.pkl",
             "TreeCounts_UNIQUE_UNREAL": f"ProcessedResults/TreeCount/UA{parameter}_UniqueTreeCount.csv",
             "TreeCounts_UNIQUE_DUREAL": f"ProcessedResults/TreeCount/DA{parameter}_UniqueTreeCount.csv",
             "TreeCounts_ALL_UNREAL": f"ProcessedResults/TreeCount/UA{parameter}_AllTreeCount.csv",
@@ -45,13 +31,26 @@ def LoadAnalyzedData(data_type, base_directory, method, parameter):
         }
 
     #### Load Data Into Dictionary ###
-    DataDictiomary = {}
+    DataDictionary = {}
     for key, RelativePath in PathTemplates.items():
-        FullPath = os.path.join(ResultsDirectory, RelativePath)
-        try:
-            DataDictiomary[key] = pd.read_csv(FullPath)
-        except FileNotFoundError:
-            print(f"File not found: {FullPath}")
-            DataDictiomary[key] = None
 
-    return DataDictiomary
+        # Path #
+        FullPath = os.path.join(ResultsDirectory, RelativePath)
+
+        # CSV Files #
+        if RelativePath.endswith(".csv"):
+            try:
+                DataDictionary[key] = pd.read_csv(FullPath)
+            except FileNotFoundError:
+                print(f"File not found: {FullPath}")
+                DataDictionary[key] = None
+
+        # PKL Files #
+        if RelativePath.endswith(".pkl"):
+            try:
+                with open(os.path.join(ResultsDirectory, RelativePath), 'rb') as file:
+                    DataDictionary[key] = pickle.load(file)
+            except FileNotFoundError:
+                print(f"File not found: {FullPath}")
+                DataDictionary[key] = None
+    return DataDictionary
